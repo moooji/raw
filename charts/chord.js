742c5d6ad83b8238570296e7071b8441d5377461
Steffen Strätz <info@wunderkuh.com>
2014-07-27 21:24:37 +0200

    Initial commit

--- charts/chord.js
+++ charts/chord.js
@@ -86,9 +86,10 @@
             .enter().append("path")
             .style("fill", function(d) { return fill(d.index); })
             .style("stroke", "#333")
-            .style("opacity", 1)
-            .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius));
-
+            .style("opacity", 0.9)
+            .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
+            .on("mouseover", fade(.1))
+            .on("mouseout", fade(1));
 
         var ticks = g.append("g").selectAll("g")
             .data(chord.groups)
@@ -122,5 +123,26 @@
             .attr("d", d3.svg.chord().radius(innerRadius))
             .style("fill", function(d) { return fill(d.target.index); })
             .style("opacity", 1);
+
+        // Returns an array of tick angles and labels, given a group.
+        function groupTicks(d) {
+            var k = (d.endAngle - d.startAngle) / d.value;
+            return d3.range(0, d.value, 1000).map(function(v, i) {
+                return {
+                    angle: v * k + d.startAngle,
+                    label: i % 5 ? null : v / 1000 + "k"
+                };
+            });
+        }
+
+        // Returns an event handler for fading a given chord group.
+        function fade(opacity) {
+            return function(g, i) {
+                svg.selectAll(".chord path")
+                    .filter(function(d) { return d.source.index != i && d.target.index != i; })
+                    .transition()
+                    .style("opacity", opacity);
+            };
+        }
     });
 })();
