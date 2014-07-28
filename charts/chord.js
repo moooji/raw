56360d6866a1c78725ae6f9a2abd863ed1f2f21f
Steffen Strätz <info@wunderkuh.com>
2014-07-28 02:49:18 +0200

    Initial commit

--- charts/chord.js
+++ charts/chord.js
@@ -6,28 +6,23 @@
         .title('Chord diagram')
         .description("Inspired by Circos")
         .thumbnail("imgs/alluvial.png")
-        .model(graph)
+        .model(graph);
 
     var width = chart.number()
         .title("Width")
         .defaultValue(1000)
-        .fitToWidth(true)
+        .fitToWidth(true);
 
     var height = chart.number()
         .title("Height")
-        .defaultValue(500)
+        .defaultValue(500);
 
-    var nodeWidth = chart.number()
-        .title("Node Width")
-        .defaultValue(10)
-
-    var sortBy = chart.list()
-        .title("Sort by")
-        .values(['size','name','automatic'])
-        .defaultValue('size')
+    var padding = chart.number()
+        .title("Padding")
+        .defaultValue(0.05);
 
     var colors = chart.color()
-        .title("Color scale")
+        .title("Color scale");
 
     chart.draw(function (selection, data){
 
@@ -82,7 +77,7 @@
             .attr("transform", "translate(" +width() / 2 + "," +height() / 2 + ")");
 
         var chord = d3.layout.chord()
-            .padding(.05)
+            .padding(+padding())
             .sortSubgroups(d3.descending)
             .matrix(chordMatrix);
 
@@ -98,7 +93,7 @@
             .enter().append("path")
             .style("fill", function(d) { return fill(d.index); })
             .style("stroke", function(d) { return fill(d.index); })
-            .style("opacity", 0.8)
+            .style("opacity", 0.6)
             .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius));
 
         var ticks = g.append("g").selectAll("g")
@@ -130,7 +125,7 @@
             .selectAll("path")
             .data(chord.chords)
             .enter().append("path")
-            .attr("d", d3.svg.chord().radius(innerRadius))
+            .attr("d", d3.svg.chord().radius(outerRadius))
             .style("fill", function(d) { return fill(d.target.index); })
             .style("opacity", 0.5);
 
